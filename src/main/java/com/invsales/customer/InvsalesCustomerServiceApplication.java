b8a88ac8439d9aa6e46dc94500938570292f7ba8
satheeshchepuri <chepurusatheesh@gmail.com>
2019-12-25 12:14:09 +0530

    first commit

---
+++ src/main/java/com/invsales/customer/InvsalesCustomerServiceApplication.java
@@ -0,0 +1,25 @@
+package com.invsales.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class InvsalesCustomerServiceApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(InvsalesCustomerServiceApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("<< App start - InvsalesCustomerServiceApplication");
+		SpringApplication.run(InvsalesCustomerServiceApplication.class, args);
+		logger.info(">> App End - InvsalesCustomerServiceApplication");
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
